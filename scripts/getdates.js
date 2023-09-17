6c5ad2a1e35ec72a8d88d03ef864d2ec1ae3f933
brunominghini <75097330+brunominghini@users.noreply.github.com>
2023-09-16 23:01:10 -0300

    L01: Your Course Home Page
    I am building my home page for this course. It will serve as a portal page, i.e., links to assignments. You will update this page each week with links to the activities and revisions as assigned.

---
+++ scripts/getdates.js
@@ -0,0 +1,39 @@
+
+
+// Function to get the current year
+function getCurrentYear() {
+    const currentYear = new Date().getFullYear();
+    return currentYear;
+}
+
+// Function to get the last modified date of the document
+function getLastModifiedDate() {
+    const lastModified = document.lastModified;
+    return lastModified;
+}
+
+// Update the copyright year in the footer
+function updateCopyrightYear() {
+    const currentYear = getCurrentYear();
+    const yearSpan = document.getElementById("currentYear");
+
+    if (yearSpan) {
+        yearSpan.textContent = currentYear;
+    }
+}
+
+// Update the last modified date in the footer
+function updateLastModifiedDate() {
+    const lastModified = getLastModifiedDate();
+    const lastModifiedSpan = document.getElementById("lastModified");
+
+    if (lastModifiedSpan) {
+        lastModifiedSpan.textContent = lastModified;
+    }
+}
+
+// Call the functions to update the copyright year and last modified date when the page loads
+window.onload = function () {
+    updateCopyrightYear();
+    updateLastModifiedDate();
+};
