449d2e8c4ae902d6ae42e2b822872148a94735ab
brunominghini <75097330+brunominghini@users.noreply.github.com>
2023-10-18 19:56:29 -0300

    Lesso05fix

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,6 +1,15 @@
-const modeButton = document.querySelector("#change-theme");
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
 
-modeButton.addEventListener("change", (e) => {
-    document.body.classList.toogle("dark", e.target.checked)
-
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("🕶️")) {
+        body.style.background = "#262238";
+        body.style.color = "#fff";
+        modeButton.textContent = "🔆";
+    } else {
+        body.style.background = "#eee";
+        body.style.color = "#262238";
+        modeButton.textContent = "🕶️";
+    }
 });
