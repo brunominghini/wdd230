d57e7af0b8f006482e97009b053c65c105cb94da
brunominghini <75097330+brunominghini@users.noreply.github.com>
2023-10-26 20:15:35 -0300

    Lesson07Home Page Enhancement

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -40,3 +40,15 @@ window.onload = function () {
     updateLastModifiedDate();
 };
 
+
+
+if (typeof (Storage) !== "undefined") {
+    if (localStorage.visitCount) {
+        localStorage.visitCount = parseInt(localStorage.visitCount) + 1;
+    } else {
+        localStorage.visitCount = 1;
+    }
+    document.getElementById("visitCount").textContent = localStorage.visitCount;
+} else {
+    alert("Browser not suported");
+}
