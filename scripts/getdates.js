e9ef449848408895a2f2d80b13439c18ed888315
brunominghini <75097330+brunominghini@users.noreply.github.com>
2023-10-26 20:49:11 -0300

    Lesson07HomePageEnhancement

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,44 +1,20 @@
-/*document.getElementById("currentYear").innerText = new Date().getFullYear;
-
-document.getElementById9("lastModified").innerText = `Last Modified: ${document.lastModified}`
-*/
-// Function to get the current year
 function getCurrentYear() {
     const currentYear = new Date().getFullYear();
     return currentYear;
 }
 
-// Function to get the last modified date of the document
+document.getElementById("currentYear").innerText = getCurrentYear();
+
 function getLastModifiedDate() {
     const lastModified = document.lastModified;
     return lastModified;
 }
 
-// Update the copyright year in the footer
-function updateCopyrightYear() {
-    const currentYear = getCurrentYear();
-    const yearSpan = document.getElementById("currentYear");
-
-    if (yearSpan) {
-        yearSpan.textContent = currentYear;
-    }
+function getLastModifiedDate() {
+    const lastModified = document.lastModified;
+    return lastModified;
 }
-
-// Update the last modified date in the footer
-function updateLastModifiedDate() {
-    const lastModified = getLastModifiedDate();
-    const lastModifiedSpan = document.getElementById("lastModified");
-
-    if (lastModifiedSpan) {
-        lastModifiedSpan.textContent = lastModified;
-    }
-}
-
-// Call the functions to update the copyright year and last modified date when the page loads
-window.onload = function () {
-    updateCopyrightYear();
-    updateLastModifiedDate();
-};
+document.getElementById("lastModified").innerText = `${document.lastModified}`
 
 
 
