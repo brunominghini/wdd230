2c2a50ec0eb283ed38a466fc1804111844a2eed6
brunominghini <75097330+brunominghini@users.noreply.github.com>
2023-11-08 21:00:36 -0300

    Lesson07fix

--- chamber/scripts/information.js
+++ chamber/scripts/information.js
@@ -15,3 +15,35 @@ function getLastModifiedDate() {
     return lastModified;
 }
 document.getElementById("lastModified").innerText = `Last Modified: ${document.lastModified}`
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const currentDate = new Date();
+
+    const lastVisit = localStorage.getItem("lastVisit");
+
+    if (lastVisit) {
+        const lastVisitDate = new Date(lastVisit);
+
+        const timeDifference = currentDate - lastVisitDate;
+
+        const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+        if (daysDifference === 0) {
+            displayMessage("Welcome! Let us know if you have any questions.");
+        } else if (daysDifference === 1) {
+            displayMessage("You last visited 1 day ago.");
+        } else {
+            displayMessage(`You last visited ${daysDifference} days ago.`);
+        }
+    } else {
+        displayMessage("Welcome! Let us know if you have any questions.");
+    }
+    localStorage.setItem("lastVisit", currentDate);
+});
+function displayMessage(message) {
+    const sidebar = document.querySelector(".sidebar");
+    const messageElement = document.createElement("p");
+    messageElement.textContent = message;
+    sidebar.appendChild(messageElement);
+}
