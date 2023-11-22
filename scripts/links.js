46455dc13c35d76622ae755dc919a53bbc7bfa23
brunominghini <75097330+brunominghini@users.noreply.github.com>
2023-11-22 20:23:21 -0300

    Lesson09

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,10 @@
 const baseURL = "https://github.com/brunominghini/wdd230/";
 const linksURL = "https://github.com/brunominghini/wdd230/data/linksURL";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+}
+
+getLinks();
