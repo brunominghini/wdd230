ef4947659086d4742e6034075b36961dc82f97f4
brunominghini <75097330+brunominghini@users.noreply.github.com>
2023-11-22 20:02:43 -0300

    Lesson09

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -17,20 +17,20 @@ const displayProphets = (prophets) => {
         let portrait = document.createElement('img');
 
         // Build the h2 content out to show the prophet's full name
-        fullName.textContent = '${prophet.name} ${prophet.lastname}';
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
         // Build the image portrait by setting all the relevant attributes
         portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', 'Portrait of ${ prophet.name } ${ prophet.lastname }');
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
 
         // Append the section(card) with the created elements
-        card.appendChild(card);
         card.appendChild(portrait);
         card.appendChild(fullName);
         cards.appendChild(card);
-
-    }); // end of arrow function and forEach loop
+    });
 }
+
+
 
