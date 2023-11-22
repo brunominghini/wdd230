6127a63a43170f4d89c972594c43caf4a30c70ed
brunominghini <75097330+brunominghini@users.noreply.github.com>
2023-11-22 20:30:16 -0300

    Lesson09

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,39 @@
 const baseURL = "https://github.com/brunominghini/wdd230/";
-const linksURL = "https://github.com/brunominghini/wdd230/data/linksURL";
+const linksURL = "https://github.com/brunominghini/wdd230/data/link.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data);
+
+}
+
+function displayLinks(weeks) {
+    const linksContainer = document.getElementById("links-container");
+
+    weeks.forEach(week => {
+        const weekElement = document.createElement("div");
+        weekElement.classList.add("week");
+
+        const weekTitle = document.createElement("h3");
+        weekTitle.textContent = `Semana ${week.week}`;
+
+        const linksList = document.createElement("ul");
+
+        week.links.forEach(link => {
+            const listItem = document.createElement("li");
+            const linkElement = document.createElement("a");
+            linkElement.href = `${baseURL}${link.url}`;
+            linkElement.textContent = link.title;
+
+            listItem.appendChild(linkElement);
+            linksList.appendChild(listItem);
+        });
+
+        weekElement.appendChild(weekTitle);
+        weekElement.appendChild(linksList);
+        linksContainer.appendChild(weekElement);
+    });
 }
 
 getLinks();
