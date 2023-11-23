64e7043dea0f30000ad71018cda7e873c183cd72
brunominghini <75097330+brunominghini@users.noreply.github.com>
2023-11-23 02:13:54 -0300

    Lesson09Directory

--- chamber/scripts/behaviors.js
+++ chamber/scripts/behaviors.js
@@ -5,3 +5,21 @@ hambutton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hambutton.classList.toggle('open');
 });
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+
+gridbutton.addEventListener("click", () => {
+
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
