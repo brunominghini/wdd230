4a5a9dc706c4932007e695cfc445341b22dd901c
brunominghini <75097330+brunominghini@users.noreply.github.com>
2023-11-23 21:49:52 -0300

    Lesson10

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,22 +1,33 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('#figcaption');
-const lat = 49.74;
-const lon = 6.63;
+const lat = 49.75;
+const lon = 6.64;
 const apiKey = '0f8b3b88198771a951f0ceacc78f784b';
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}';
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
 async function apiFetch() {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data); // testing only
-            // displayResults(data); // uncomment when ready
+            console.log(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
     } catch (error) {
         console.log(error);
     }
+}
+apiFetch();
+
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`; // Ajustado para acessar a temperatura corretamente
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`; // Ajustado o formato da imagem (era '.ong')
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc; // Ajustado para exibir a descrição diretamente
 }
