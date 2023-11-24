0a8a467f19e8bb9e4c9e27aa6d0b19f4e1883a7c
brunominghini <75097330+brunominghini@users.noreply.github.com>
2023-11-23 22:20:28 -0300

    Lesson10Weather

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,33 +1,35 @@
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('#figcaption');
-const lat = -23.27;
-const lon = -46.75;
-const apiKey = '0f8b3b88198771a951f0ceacc78f784b';
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+document.addEventListener('DOMContentLoaded', () => {
+    const currentTemp = document.querySelector('#current-temp');
+    const weatherIcon = document.querySelector('#weather-icon');
+    const captionDesc = document.querySelector('#figcaption');
+    const lat = -23.27;
+    const lon = -46.75;
+    const apiKey = '0f8b3b88198771a951f0ceacc78f784b';
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
-async function apiFetch() {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-            displayResults(data);
-        } else {
-            throw Error(await response.text());
+    async function apiFetch() {
+        try {
+            const response = await fetch(url);
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                displayResults(data);
+            } else {
+                throw Error(await response.text());
+            }
+        } catch (error) {
+            console.log(error);
         }
-    } catch (error) {
-        console.log(error);
     }
-}
 
-function displayResults(data) {
-    currentTemp.textContent = `${data.main.temp} °F`;
-    captionDesc.textContent = data.weather[0].description;
-    locationElement.textContent = data.name;
-    const iconCode = data.weather[0].icon;
-    const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
-    weatherIcon.src = iconUrl;
-}
+    function displayResults(data) {
+        currentTemp.textContent = `${data.main.temp} °F`;
+        captionDesc.textContent = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+        weatherIcon.src = iconUrl;
+    }
 
-apiFetch();
+    apiFetch();
+});
+
