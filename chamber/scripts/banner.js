2c07068a8fd412ac6396a343abb4f4c537a606f9
brunominghini <75097330+brunominghini@users.noreply.github.com>
2023-11-26 00:23:18 -0300

    Lesson10banner

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -7,7 +7,7 @@ function showOrHideBanner() {
     const banner = document.getElementById('banner');
 
     // Show banner only on Mon (1), Tue (2), Wed (3), and Sat (6)
-    if ([0, 1, 2, 3,].includes(currentDay)) {
+    if ([1, 2, 3,].includes(currentDay)) {
         banner.style.display = 'block';
     } else {
         banner.style.display = 'none';
