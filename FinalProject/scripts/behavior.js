d06d911180a3755bef3d3a21091c6393ece904f9
brunominghini <brunoaminghini@outlook.com.br>
2023-12-11 22:06:19 -0300

    Lesson13

--- FinalProject/scripts/behavior.js
+++ FinalProject/scripts/behavior.js
@@ -4,4 +4,21 @@ const navigation = document.querySelector('.navigation');
 hambutton.addEventListener('click', () => {
     navigation.classList.toggle('open');
 });
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+
+gridbutton.addEventListener("click", () => {
+
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
 
