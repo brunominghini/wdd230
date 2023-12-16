35311b7a0c2689874e22c64f218f5dc3d9435a45
brunominghini <brunoaminghini@outlook.com.br>
2023-12-16 15:00:25 -0300

    Lesson14

--- FinalProject/scripts/darkmode.js
+++ FinalProject/scripts/darkmode.js
@@ -1,21 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const modeButton = document.querySelector("#mode");
+  const body = document.querySelector("body");
+  const header = document.querySelector("header");
+  const footer = document.querySelector("footer");
 
-document.addEventListener("DOMContentLoaded", function() {
-    const modeButton = document.querySelector("#mode");
-    const body = document.querySelector("body");
-    const header = document.querySelector("header");
-    const footer = document.querySelector("footer");
-    
-    modeButton.addEventListener("click", () => {
-        if (modeButton.textContent.includes("🕶️")) {
-            body.style.background = "#262238";
-            body.style.color = "#d3d3d3";
-            footer.style.background = "#080808";
-            modeButton.textContent = "🔆";
-        } else {
-            body.style.background = "#d3d3d3";
-            body.style.color = "#262238";
-            modeButton.textContent = "🕶️";
-            footer.style.background = "#fff";
-        }
-    });
-    });
+  modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("🕶️")) {
+      body.style.background = "#262238";
+      body.style.color = "#d3d3d3";
+      footer.style.background = "#080808";
+      modeButton.textContent = "🔆";
+    } else {
+      body.style.background = "#d3d3d3";
+      body.style.color = "#262238";
+      modeButton.textContent = "🕶️";
+      footer.style.background = "#fff";
+    }
+  });
+});
